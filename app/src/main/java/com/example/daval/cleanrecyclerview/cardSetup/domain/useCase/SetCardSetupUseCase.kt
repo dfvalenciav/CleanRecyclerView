596b5ba47e71853d0fa81c49f2a0d9367636518e
Daniel Valencia <dfvalenciav@gmail.com>
2022-08-29 15:21:11 +0200

    autosizing refactoring xml

--- app/src/main/java/com/example/daval/cleanrecyclerview/cardSetup/domain/useCase/SetCardSetupUseCase.kt
+++ app/src/main/java/com/example/daval/cleanrecyclerview/cardSetup/domain/useCase/SetCardSetupUseCase.kt
@@ -11,20 +11,8 @@ import javax.inject.Inject
 
 class SetCardSetupUseCase @Inject constructor(
     private val repository: ICardRealmRepository
-): BaseUseCase<CardSetup>(), ICardListPreferred {
-
-    val datapassed : CardSetupPresentation = CardSetupPresentation("Tarjeta de crédito", "1234567890", true)
-    val selectedCard : List<CardSetup> = mutableListOf(
-        CardSetup("Tarjeta de crèdito", "1234567890",false,10000,"Pedro Pèrez", 321567765),
-        CardSetup("Tarjeta de dèbito", "3855695039",false,20000,"Pedro Pèrez", 321567765))
+): BaseUseCase<CardSetup>() {
     override suspend fun execute(): List<CardSetup> {
-        addPreferredCard(datapassed)
-        return selectedCard
-    }
-
-    override fun addPreferredCard(datapassed: CardSetupPresentation)   {
-        Log.d("PRUEBA", selectedCard.size.toString())
-        val preferredCard = CardSetup(datapassed.cardListType?:"", datapassed.cardListNumber?:"", datapassed.cardListPrefered?:true, 1, "Pedro Pérez", 321567765)
-        selectedCard.plusElement(preferredCard)
+        return repository.getCardListSetupObjects()
     }
 }
